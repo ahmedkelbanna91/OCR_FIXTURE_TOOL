b96090a9427e14e38cf713adbf898b7b4dbaa21b
Ahmed ElBanna <ahmedkelbanna91@gmail.com>
2024-05-24 21:31:12 +0200

    Initial commit

---
+++ OCR_FIXTURE_TOOL/OCR_FIXTURE_TOOL.cpp
@@ -0,0 +1,174 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
+#include <CGAL/Polygon_mesh_processing/corefinement.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/bounding_box.h>
+#include <CGAL/centroid.h>
+#include <fstream>
+#include <algorithm> 
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <filesystem>
+
+
+
+namespace PMP = CGAL::Polygon_mesh_processing;
+namespace fs = std::filesystem;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef CGAL::Surface_mesh<Kernel::Point_3> Mesh;
+typedef Kernel::Point_3 Point;
+typedef Kernel::Vector_3 Vector;
+
+void get_model_dimensions(Mesh mesh, float& modelWidth, float& modelLength, float& modelHeight) {
+	std::vector<Point> points;
+	for (auto v : mesh.vertices()) {
+		points.push_back(mesh.point(v));
+	}
+	Kernel::Iso_cuboid_3 bbox = CGAL::bounding_box(points.begin(), points.end());
+	modelWidth = static_cast<float>(bbox.xmax() - bbox.xmin());
+	modelLength = static_cast<float>(bbox.ymax() - bbox.ymin());
+	modelHeight = static_cast<float>(bbox.zmax() - bbox.zmin());
+}
+
+void compute_centroid(Mesh mesh, Point& centroid) {
+	std::vector<Point> vertices;
+	for (auto v : mesh.vertices()) {
+		vertices.push_back(mesh.point(v));
+	}
+	centroid = CGAL::centroid(vertices.begin(), vertices.end());
+}
+
+void scaleMesh(Mesh& mesh, float XYscale, float Zscale, float zThreshold, float XYtopscale) {
+	for (auto v : mesh.vertices()) {
+		Point& p = mesh.point(v);
+		float new_x, new_y, new_z;
+
+		if (p.z() > zThreshold) {
+			new_x = p.x() * XYtopscale;
+			new_y = p.y() * XYtopscale;
+		}
+		else {
+			new_x = p.x() * XYscale;
+			new_y = p.y() * XYscale;
+		}
+		new_z = p.z() * Zscale;
+
+		mesh.point(v) = Point(new_x, new_y, new_z);
+	}
+}
+
+void translate_mesh(Mesh& mesh, const Vector& translation_vector) {
+	for (auto v : mesh.vertices()) {
+		mesh.point(v) = mesh.point(v) + translation_vector;
+	}
+}
+
+bool read_STL(const std::string& filename, Mesh& mesh) {
+	fs::path filepath(filename);
+	mesh.clear();
+	if (!PMP::IO::read_polygon_mesh(filename, mesh)) {
+		std::cerr << "Error: Cannot read the STL file " << filepath.filename() << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool write_STL(const std::string& filename, const Mesh& mesh) {
+	fs::path filepath(filename);
+	if (!CGAL::IO::write_polygon_mesh(filename, mesh, CGAL::parameters::stream_precision(10))) {
+		std::cerr << "Error: Cannot write the STL file: " << filepath.filename() << std::endl;
+		return false;
+	}
+	return true;
+}
+
+int main(int argc, char* argv[]) {
+	bool lastWasDigit = false;
+	float offsetX = 0.0f, offsetY = 0.0f, offsetZ = 0.0f;
+	float XYscale = 0.18f, XYtopscale = 0.18f, Zscale = 0.30f;
+	float zThreshold = 0.1f;
+	float Xspacing = 0.8f, Yspacing = 2.9f;
+	float Xtranslate = -6.5f, Ytranslate = -7.5f, zDepth= 4.0f; //2.6f
+
+	std::map<std::string, std::string> args;
+	for (int i = 1; i < argc; ++i) {
+		if (i + 1 < argc) {
+			args[argv[i]] = argv[i + 1];
+			i++;
+		}
+		else {
+			std::cerr << "Missing value for " << argv[i] << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+
+	if (args.find("-O") == args.end() || args.find("-N") == args.end() || args.find("-D") == args.end()) {
+		std::cerr << "Usage: OCR_FIXTURE_TOOL.exe -O out.stl -N id -D Depth [-I model.stl]    (V3.0 CreatedByBanna)" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string Output_Path = args["-O"], ID = args["-N"], Model_Path = args["-I"];
+	zDepth = 4.0f + std::atof(args["-D"].c_str());
+
+	std::transform(ID.begin(), ID.end(), ID.begin(), [](unsigned char c) { return std::toupper(c); });
+
+	Mesh Letter_Mesh, Tag_Mesh, Fixture_Mesh, Model_Mesh, Result_Mesh;
+
+	for (char c : ID) {
+		float FontWidth = 0.0f, FontLength = 0.0f, FontHeight = 0.0f;
+		std::string filename = "models/font/" + std::string(1, c) + ".stl";
+
+		if (!read_STL(filename, Letter_Mesh))  return EXIT_FAILURE;
+
+		get_model_dimensions(Letter_Mesh, FontWidth, FontLength, FontHeight);
+
+		if (std::isdigit(c)) {
+			lastWasDigit = true;
+		}
+		else if (lastWasDigit) {
+			offsetY -= (FontLength * XYscale) + Yspacing;
+			offsetX = 0.15f;
+			lastWasDigit = false;
+		}
+		scaleMesh(Letter_Mesh, XYscale, Zscale, zThreshold, XYtopscale);
+		translate_mesh(Letter_Mesh, Kernel::Vector_3(offsetX, offsetY, offsetZ));
+		offsetX += (FontWidth * XYscale) + Xspacing;
+		CGAL::copy_face_graph(Letter_Mesh, Tag_Mesh);
+	}
+
+	translate_mesh(Tag_Mesh, Kernel::Vector_3(Xtranslate, Ytranslate, zDepth));
+
+	if (!read_STL("models/fixture.stl", Fixture_Mesh)) return EXIT_FAILURE;
+
+	if (!PMP::corefine_and_compute_difference(Fixture_Mesh, Tag_Mesh, Result_Mesh)) {
+		std::cerr << "Error: Subtraction operation failed." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+
+	if (!Model_Path.empty()) {
+		if (!read_STL(Model_Path, Model_Mesh)) return EXIT_FAILURE;
+		
+		Point centroid;
+		PMP::orient(Model_Mesh);
+		compute_centroid(Model_Mesh, centroid);
+		translate_mesh(Model_Mesh, Kernel::Vector_3(-centroid.x(), -centroid.y() + 7, 0));
+
+		//CGAL::copy_face_graph(Model_Mesh, Result_Mesh);
+		Mesh Sub_Mesh = Result_Mesh;
+		Result_Mesh.clear();
+
+		if (!PMP::corefine_and_compute_union(Model_Mesh, Sub_Mesh, Result_Mesh)) {
+		    std::cerr << "Error: Addition operation failed." << std::endl;
+		    return EXIT_FAILURE;
+		}
+	}
+
+	if (!write_STL(Output_Path, Result_Mesh)) return EXIT_FAILURE;
+
+	std::cout << "      Operation completed successfully." << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# OCR_FIXTURE_TOOL
+ This tool creates the fixture with OCR intruded (engraved) text then combines the resut to an input model, the tool keeps the fixture's origin , but it moves the input model to the fixture's origin
